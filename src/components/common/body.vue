d7f595b67842ec16f6a3567ca764a175e0305c92
yeyuguo <346243440@qq.com>
2018-04-09 22:06:40 +0800

    modified:   src/components/common/body.vue

--- src/components/common/body.vue
+++ src/components/common/body.vue
@@ -1,20 +1,27 @@
 <template>
   <div id="body">
-      <div v-text="props_msg">{{props_msg}}</div>
+      <div v-text="props_msg"></div>
+      <div v-text="data_msg"></div>
+      <div>{{compute_msg}}</div>
   </div>
 </template>
 
 
 <script>
 export default {
-  name:'Body',
-  props:['props_msg'],
-  data(){
-      return {
-          msg:"这是内容部分"
-      }
+  name: "Body",
+  props: ["props_msg"],
+  data() {
+    return {
+      data_msg: `这是 data 获取来的属性值 '${this.props_msg}'`
+    };
+  },
+  computed: {
+    compute_msg: function() {
+      return `这是 props 获取来的属性值 '${this.props_msg}'`;
+    }
   }
-}
+};
 </script>
 
 
